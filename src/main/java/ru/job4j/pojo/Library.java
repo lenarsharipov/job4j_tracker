3acb5660852e8ece33ebc9023315d533d5648370
lenarsharipov <jHuMeY2YStt4km6>
2021-08-01 12:48:27 +0300

    3. Массивы и модели. [#174892 #184067]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -16,25 +16,23 @@ public class Library {
 
     public static void print(Book[] array) {
         for (int index = 0; index < array.length; index++) {
-            System.out.println("Title : " +  array[index].getTitle() + System.lineSeparator()
+            System.out.println("Title : " + array[index].getTitle() + System.lineSeparator()
                              + "Pages : " + array[index].getPageCount() + System.lineSeparator());
         }
     }
 
     public static Book[] swap(Book[] array, int src, int dest) {
-        Book temp = new Book(array[src].getTitle(), array[src].getPageCount());
-        array[src].setTitle(array[dest].getTitle());
-        array[src].setPageCount(array[dest].getPageCount());
-        array[dest].setTitle(temp.getTitle());
-        array[dest].setPageCount(temp.getPageCount());
+        Book temp = array[src];
+        array[src] = array[dest];
+        array[dest] = temp;
         return array;
     }
 
     public static void filteredPrint(Book[] array, String title) {
         for (int index = 0; index < array.length; index++) {
             if (array[index].getTitle().equals(title)) {
-                System.out.println("Title : " +  array[index].getTitle() + System.lineSeparator()
-                        + "Pages : " + array[index].getPageCount() + System.lineSeparator());
+                System.out.println("Title : " + array[index].getTitle() + System.lineSeparator()
+                                 + "Pages : " + array[index].getPageCount() + System.lineSeparator());
             }
         }
     }
