21c10e54c9db6a8895721ea6ac50e6d926185f3c
lenarsharipov <JBKPR6C9L35rCHq>
2022-06-28 17:54:08 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,58 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (!users.containsKey(user)) {
+            users.put(user, new ArrayList<Account>());
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+        if (findByPassport(passport) != null
+            && !users.get(findByPassport(passport)).contains(account)) {
+            users.get(findByPassport(passport)).add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (passport.equals(user.getPassport())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        if (findByPassport(passport) != null) {
+            List<Account> accounts = users.get(findByPassport(passport));
+            for (Account account : accounts) {
+                if (requisite.equals(account.getRequisite())) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        if (findByRequisite(srcPassport, srcRequisite) != null
+            && findByRequisite(srcPassport, srcRequisite).getBalance() >= amount) {
+            findByRequisite(srcPassport, srcRequisite).
+                    setBalance(findByRequisite(srcPassport, srcRequisite).getBalance() - amount);
+            findByRequisite(destPassport, destRequisite).
+                    setBalance(findByRequisite(destPassport, destRequisite).getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
