653417cd76244d9b09dc5033f25d3207f0185f9e
lenarsharipov <JBKPR6C9L35rCHq>
2022-06-29 15:53:51 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ItemAscByNameTest {
+
+    @Test
+    public void compareItemsAscByName() {
+        List<Item> items = Arrays.asList(
+                new Item(1, "ccc"),
+                new Item(2, "aaa"),
+                new Item(3, "bbb")
+        );
+        items.sort(new ItemAscByName());
+        List<Item> expected = Arrays.asList(
+                new Item(2, "aaa"),
+                new Item(3, "bbb"),
+                new Item(1, "ccc")
+        );
+        assertEquals(expected, items);
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ItemDescByNameTest {
+
+    @Test
+    public void compareItemsDescByName() {
+        List<Item> items = Arrays.asList(
+                new Item(1, "aaa"),
+                new Item(2, "ccc"),
+                new Item(3, "bbb")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item(2, "ccc"),
+                new Item(3, "bbb"),
+                new Item(1, "aaa")
+        );
+        items.sort(new ItemDescByName());
+        assertEquals(expected, items);
+    }
+}
