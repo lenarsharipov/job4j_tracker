b58879f15a3f7e3c9651687d87c27a259ac10813
lenarsharipov <JBKPR6C9L35rCHq>
2022-06-29 16:44:02 +0300

    0. Сортировка [#10096]

--- src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -24,4 +24,5 @@ public class ItemAscByNameTest {
         );
         assertEquals(expected, items);
     }
+
 }
--- src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -24,4 +24,5 @@ public class ItemDescByNameTest {
         );
         assertEquals(expected, items);
     }
+
 }
