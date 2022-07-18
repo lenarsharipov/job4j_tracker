7ed57f246447b87ead3381f9ce89252f2f2923f2
lenarsharipov <lenarsharipov@gmail.com>
2022-07-18 11:33:35 +0300

    2.0. Лямбда [#58165]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,30 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public static void main(String[] args) {
+        Attachment[] atts = {
+                new Attachment("image 1", 20),
+                new Attachment("image 2", 120),
+                new Attachment("image 3", 23)
+        };
+        Comparator<Attachment> comparator = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return Integer.compare(left.getSize(), right.getSize());
+            }
+        };
+        Comparator<Attachment> comparator2 = (left, right) ->
+                Integer.compare(left.getSize(), right.getSize());
+
+        Comparator<String> cmpSize = (left, right) ->
+                Integer.compare(left.length(), right.length());
+
+        Comparator<String> cmpText = (left, right) ->
+                left.compareTo(right);
+        Comparator<String> cmpDescSize = (left, right) ->
+                Integer.compare(right.length(), left.length());
+    }
+}
