6dc2098c270022c948a1398fa13c05121a85a508
lenarsharipov <lenarsharipov@gmail.com>
2023-04-07 16:15:47 +0300

    1. ToMany [#301848]

--- src/main/java/ru/job4j/toone/User.java
+++ src/main/java/ru/job4j/toone/User.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.EqualsAndHashCode.Include;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -20,4 +23,13 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+
+    /**
+     * Для связи one-to-many обязательно нужно указывать колонку
+     * для вторичного ключа. Если это не сделать, то hibernate
+     * будет создавать отдельную таблицу, а не использовать нашу схему.
+     */
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "j_user_id")
+    private List<UserMessenger> messengers = new ArrayList<>();
 }
