d720a6d4858475b4f367495e6cdddcf8bc415b82
lenarsharipov <lenarsharipov@gmail.com>
2023-04-08 10:53:11 +0300

    2. ManyToMany [#331986]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,13 @@
 package ru.job4j.tracker;
 
 import lombok.*;
+import ru.job4j.toone.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "items")
@@ -33,14 +36,33 @@ public class Item {
 
     private LocalDateTime created = LocalDateTime.now();
 
+    @ManyToMany
+    @JoinTable(/* Разберем аннотацию @JoinTable */
+            /* name - указывает на таблицу, где идет связь вторичных ключей. */
+            name = "participates",
+
+            /* joinColumns - определяет ключ родительского объекта. В данном примере Item.id */
+            joinColumns = { @JoinColumn(name = "item_id") },
+
+            /* inverseJoinColumns - определяет ключ объекта,
+            который мы загружаем в родительский объект. */
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    private List<User> participates = new ArrayList<>();
+
     public Item(int id, @NonNull String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Item(int id, @NonNull String name, LocalDateTime created) {
+        this.id = id;
+        this.name = name;
+        this.created = created;
     }
 
     @ToString.Include
     String created() {
         return FORMATTER.format(created);
     }
-
 }
